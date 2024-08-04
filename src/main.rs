cbbc6e937c52d737c76d4b48acda0ee09f1ddd19
Elizbeth <elizabethadebayo269@gmail.com>
2024-08-04 22:21:18 +0100

    geting user input

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,8 @@
-
+use std::io;
+fn main() {
+    let mut user_input = String::new();
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("couldn't read line");
+    println!("{user_input}");
+}
